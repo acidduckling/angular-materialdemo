866dcdb41fbed15209f7dbcbe5750a4bbdf663fd
CLayton Wakefield <acidduckling@gmail.com>
2018-05-11 22:33:41 +1000

    226 - Creating a reusable module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // If not using animations, use the following import instead:
 // import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {
-  MatCheckboxModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatInputModule,
-  MatSlideToggleModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  NativeDateAdapter,
-  MatIconModule,
-  MatButtonModule,
-  MatChipsModule,
-  MatProgressSpinnerModule,
-  MatTooltipModule,
-  MatTabsModule,
-  MatDialogModule
-} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { EditCourseComponent } from './edit-course/edit-course.component';
+import { MatComponentsModule } from './mat-components.module';
 
 @NgModule({
   declarations: [AppComponent, EditCourseComponent],
@@ -34,19 +18,7 @@ import { EditCourseComponent } from './edit-course/edit-course.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatTabsModule,
-    MatDialogModule
+    MatComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
